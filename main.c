ed07b254a6150e65354c349c52accbae197273ef
Todd <yanggt97@gmail.com>
2017-09-28 22:22:03 -0400

    Added: tagArray[setnumber][how_many_line_per_set] lruArray[setnumber][how_many_line_per_set] update on miss

--- main.c
+++ main.c
@@ -8,9 +8,14 @@
 //define vars
 //Max File Size
 #define max_str_len             1000
+<<<<<<< HEAD
 int **tagArray;
 int **lruArray;
+int manySet;//how many set
+int manyLine;//how many line
 
+=======
+>>>>>>> origin/master
 
   char* hexTobinary(char *hexa, char *binarynum){
     int i =0;
@@ -93,55 +98,83 @@ int **lruArray;
     binarynum[nbytes] = '\0';
     return binarynum;
 }
-int power2 (u_int32_t x){
-    unsigned int bits;
-    while(x>0){
-        x=x>>1;
+
+
+int nbits (u_int32_t x){
+    int n = x-1;
+    int bits=0;
+    while(n>0){
+        n=n>>1;
         bits+=1;
     }
     return bits;
 }
 
 int whichSet(u_int32_t x,u_int32_t C,u_int32_t L,u_int32_t K ){
-    u_int32_t  addr=x;
-    addr>>offsetLength(L);
+    int offset =setIndexLength(C,L,K);
+    u_int32_t  addr=x>>offset;
     int mask =0;
-    for (int i =0, i<setIndexLength(C,L,K),mask
-    while(i<setIndexLength(C,L,K)){
-
+    int i = 0;
+    while(i<offset){
+        mask =mask<<1 | 1;
+        i++;
     }
+    printf("%x\n",mask);
+    return  addr & mask;
 }
-
 int offsetLength(u_int32_t L){
-   return power2 (L);
+    return nbits (L);
 }
 
 int setIndexLength(u_int32_t C,u_int32_t L,u_int32_t K){
     u_int32_t nSet = C/L/K;
-    return power2 (nSet);
+    return nbits (nSet);
 }
-
 int tagBits(u_int32_t x ,u_int32_t C,u_int32_t L,u_int32_t K){
     u_int32_t  addr=x;
-    addr>> (setIndexLength(C,L,K)+offsetLength(L));
+    addr =addr >> (setIndexLength(C,L,K)+offsetLength(L));
     return addr;
 }
 
-void updateOnMiss(u_int32_t tag, u_int32_t x, u_int32_t C,u_int32_t L,u_int32_t K ){
-    int setNum= whichSet();//waiting for whichset()
+<<<<<<< HEAD
+int setaddress(u_int32_t x ,u_int32_t C,u_int32_t L,u_int32_t K){
+    int lineNum = x<<(31-offsetLength(L)-setIndexLength(C,L,K));//get rid of tag
+    lineNum = lineNum >> (31-offsetLength(L));//shifting set address to the right
+    int a=0x0111111>>(30-offsetLength(L));
+    lineNum=lineNum&a;//get rid of 2s complement
+    return lineNum;
+}
+
+void updateOnMiss(u_int32_t tag, u_int32_t x, u_int32_t C,u_int32_t L,u_int32_t K ){//tag, address, C, L, K
+    int setNum = whichSet();//waiting for whichset()
+    //get set address
+    int line=setaddress(x,C,L,K)-L*setNum+1;//getting which line, L start with 1
+    MissTag(setNum,line, tag);//updatedata
+    MissLru(setNum,line,manySet,manyLine);//updatedata
 
 }
-void MissLru(){}
-
-void MissTag(int set, int line){
-
+void MissLru(int set, int line, int manySet, int manyLine){
+    for(int j=0; j<manySet; j++){//set# sweep
+        for(int i=0; i<manyLine; i++){//line# sweep
+            if((j!=set)&&(i!=line)){//lruArray++ if this line not been called
+                lruArray[j][i]++;
+            }
+        }
+    }
 }
+
+void MissTag(int set, int line, int tag){
+    tagArray[set][line]=tag;
+}
+=======
+>>>>>>> origin/master
 int main(int argc, char *argv[]) {
-    argv takes [0]main.c [1]K, [2]L,[3]C [4]traceFile
-    int K = int(argv[1]);
-    int L = int(argv[2]);
-    int C = int(argv[3]);//shabi, this is KB
+    //argv takes [0]main.c [1]K, [2]L,[3]C [4]traceFile
+    //int K = int(argv[1]) ;
+    //int L = int(argv[2]);
+    //int C = int(argv[3])*1000;
     sprintf(argv[4],"C:\\Users\\haoga\\OneDrive\\com.sys\\sampleTrace.txt");
+<<<<<<< HEAD
     char *tracefile = argv[4];
 
     //bit shift
@@ -150,6 +183,8 @@ int main(int argc, char *argv[]) {
 
     //cache structure
     int set=C*1000/(L*K)-1;//how many set in cash
+    manySet=set;
+    manyLine=K;
     tagArray=(int**)malloc((set+1)*max_str_len);//[set][line]
     for(int j=0;j<set+1;j++){
         *(tagArray+j)=(int*)malloc(K*sizeof(int));//each set has K line
@@ -159,9 +194,13 @@ int main(int argc, char *argv[]) {
     for(int j=0;j<set+1;j++){
         *(lruArray+j)=(int*)malloc(K*sizeof(int));//each set has K line
     }
+=======
+    char tracefile [] = argv[4];
+>>>>>>> origin/master
 
+    //trace
     char hexa [max_str_len];
-    //char binarynum[max_str_len];
+    u_int32_t decimal ;
 
     //Open file
     FILE *file;
@@ -175,8 +214,8 @@ int main(int argc, char *argv[]) {
     //scan traces and access cache
     while(fscanf(file,"%s",&hexa[0])!=EOF){
         printf("hex : %s\n",hexa);
-        hexTobinary(hexa,binarynum);
-        printf("binary :%s\n",binarynum);
+        u_int32_t decimal =(u_int32_t)strtol(hexa, NULL, 16);
+        printf("decimal : %u\n",decimal);
     }
 
 
