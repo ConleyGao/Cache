aa5eea32332727f535bcd621d67cef723a793d0b
ConleyGao <haogao@umass.edu>
2017-09-28 22:57:11 -0400

    remove htob

--- main.c
+++ main.c
@@ -4,93 +4,9 @@
 #include <math.h>
 #include <string.h>
 
-
 //define vars
 //Max File Size
 #define max_str_len             1000
-
-  /*char* hexTobinary(char *hexa, char *binarynum){
-    int i =0;
-    int nbytes = 0;
-    while(hexa[i]){
-        switch (hexa[i])
-        {
-            case '0':
-                nbytes += sprintf(binarynum+nbytes,"0000" );
-                break;
-            case '1':
-                nbytes += sprintf(binarynum+nbytes,"0001" );
-                break;
-            case '2':
-                nbytes += sprintf(binarynum+nbytes,"0010" );
-                break;
-            case '3':
-                nbytes += sprintf(binarynum+nbytes,"0011" );
-                break;
-            case '4':
-                nbytes += sprintf(binarynum+nbytes,"0100" );
-                break;
-            case '5':
-                nbytes += sprintf(binarynum+nbytes,"0101" );
-                break;
-            case '6':
-                nbytes += sprintf(binarynum+nbytes,"0110" );
-                break;
-            case '7':
-                nbytes += sprintf(binarynum+nbytes,"0111" );
-                break;
-            case '8':
-                nbytes += sprintf(binarynum+nbytes,"1000" );
-                break;
-            case '9':
-                nbytes += sprintf(binarynum+nbytes,"1001" );
-                break;
-            case 'A':
-                nbytes += sprintf(binarynum+nbytes,"1010" );
-                break;
-            case 'B':
-                nbytes += sprintf(binarynum+nbytes,"1011" );
-                break;
-            case 'C':
-                nbytes += sprintf(binarynum+nbytes,"1100" );
-                break;
-            case 'D':
-                nbytes += sprintf(binarynum+nbytes,"1101" );
-                break;
-            case 'E':
-                nbytes += sprintf(binarynum+nbytes,"1110" );
-                break;
-            case 'F':
-                nbytes += sprintf(binarynum+nbytes,"1111" );
-                break;
-            case 'a':
-                nbytes += sprintf(binarynum+nbytes,"1010" );
-                break;
-            case 'b':
-                nbytes += sprintf(binarynum+nbytes,"1011" );
-                break;
-            case 'c':
-                nbytes += sprintf(binarynum+nbytes,"1100" );
-                break;
-            case 'd':
-                nbytes += sprintf(binarynum+nbytes,"1101" );
-                break;
-            case 'e':
-                nbytes += sprintf(binarynum+nbytes,"1110" );
-                break;
-            case 'f':
-                nbytes += sprintf(binarynum+nbytes,"1111" );
-                break;
-            default:
-                printf("\n Invalid hex input %c ", hexa[i]);
-                return 0;
-        }
-        i++;
-    }
-    binarynum[nbytes] = '\0';
-    return binarynum;
-}
-*/
 
 int nbits (u_int32_t x){
     int n = x-1;
@@ -129,13 +45,10 @@ int tagBits(u_int32_t x ,u_int32_t C,u_int32_t L,u_int32_t K){
 }
 
 int main(int argc, char *argv[]) {
-    printf("te");
     //argv takes [0]main.c [1]K, [2]L,[3]C [4]traceFile
     //int K = int(argv[1]) ;
     //int L = int(argv[2]);
     //int C = int(argv[3])*1000;
-  //  sprintf(argv[4],"D:\\Downloads\\sampleTrace.txt");
-   // char tracefile [] = argv[4];
 
     //trace
     char hexa [max_str_len];
